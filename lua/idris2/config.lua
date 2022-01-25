ae79c304dc2e13aa9f6bf763a57ace77add2f328
Denis Buzdalov <public@buzden.ru>
2022-01-25 23:09:09 +0300

    [ breaking ] Make hover split position be in the hover config section

--- lua/idris2/config.lua
+++ lua/idris2/config.lua
@@ -7,12 +7,12 @@ local defaults = {
       use_split = false,
       split_size = '30%',
       auto_resize_split = false,
+      split_position = 'bottom',
       with_history = false,
     },
   },
   -- opts for nvim-lspconfig
   server = {},
-  hover_split_position = 'bottom',
   autostart_semantic = true,
   use_default_semantic_hl_groups = true,
 }
