df8052c6fa5c3246045e09140b2a194bac87f8f9
Giuseppe Lomurno <lomurno.giuseppe97@gmail.com>
2022-10-26 01:22:46 +0200

    Fix for single code actions

--- lua/idris2/code_action.lua
+++ lua/idris2/code_action.lua
@@ -41,13 +41,6 @@ function M.introspect_filter(action)
   end
 end
 
-local function has_multiple_results(filter)
-  return filter == M.filters.EXPR_SEARCH
-           or filter == M.filters.GEN_DEF
-           or filter == M.filters.REF_HOLE
-           or filter == M.filters.INTRO
-end
-
 local function handle_code_action_post_hook(action)
   local optional_post_hook = plugin_config.options.code_action_post_hook
   if optional_post_hook ~= nil then
@@ -55,54 +48,9 @@ local function handle_code_action_post_hook(action)
   end
 end
 
-local function single_action_handler(err, result, ctx, config)
+local function on_results(err, results, ctx, config)
   if err ~= nil then
-    vim.notify(err, vim.log.levels.ERROR)
-    return
-  end
-
-  if not result or #result == 0 then
-    vim.notify('No code actions available', vim.log.levels.INFO)
-    return
-  end
-
-  if #result > 1 then
-    vim.notify('Received code action with multiple results', vim.log.levels.ERROR)
-    return
-  end
-
-  local action = result[1]
-
-  if action.edit ~= nil then
-    vim.lsp.util.apply_workspace_edit(action.edit, 'utf-32')
-  end
-
-  handle_code_action_post_hook(action)
-end
-
-function M.request_single(filter)
-  local params = vim.lsp.util.make_range_params()
-  params['context'] = { diagnostics = {}, only = { filter } }
-
-  if has_multiple_results(filter) then
-    vim.lsp.buf.code_action(params.context)
-  else
-    vim.lsp.buf_request(0, 'textDocument/codeAction', params, single_action_handler)
-  end
-end
-
-function M.case_split()   M.request_single(M.filters.CASE_SPLIT)  end
-function M.make_case()    M.request_single(M.filters.MAKE_CASE)   end
-function M.make_with()    M.request_single(M.filters.MAKE_WITH)   end
-function M.make_lemma()   M.request_single(M.filters.MAKE_LEMMA)  end
-function M.add_clause()   M.request_single(M.filters.ADD_CLAUSE)  end
-function M.expr_search()  M.request_single(M.filters.EXPR_SEARCH) end
-function M.generate_def() M.request_single(M.filters.GEN_DEF)     end
-function M.intro()        M.request_single(M.filters.INTRO)       end
-
-local function on_with_hints_results(err, results, ctx, config)
-  if err ~= nil then
-    vim.notify(err, vim.log.levels.ERROR)
+    vim.notify(err.message, vim.log.levels.ERROR)
     return
   end
 
@@ -133,6 +81,21 @@ local function on_with_hints_results(err, results, ctx, config)
     }, apply_action)
   end
 end
+
+function M.request_single(filter)
+  local params = vim.lsp.util.make_range_params()
+  params['context'] = { diagnostics = {}, only = { filter } }
+  vim.lsp.buf_request(0, 'textDocument/codeAction', params, on_results)
+end
+
+function M.case_split()   M.request_single(M.filters.CASE_SPLIT)  end
+function M.make_case()    M.request_single(M.filters.MAKE_CASE)   end
+function M.make_with()    M.request_single(M.filters.MAKE_WITH)   end
+function M.make_lemma()   M.request_single(M.filters.MAKE_LEMMA)  end
+function M.add_clause()   M.request_single(M.filters.ADD_CLAUSE)  end
+function M.expr_search()  M.request_single(M.filters.EXPR_SEARCH) end
+function M.generate_def() M.request_single(M.filters.GEN_DEF)     end
+function M.intro()        M.request_single(M.filters.INTRO)       end
 
 local hints_popup_options = {
   relative = "cursor",
@@ -168,7 +131,7 @@ function M.refine_hole()
           hint = value,
         }},
       }
-      vim.lsp.buf_request(0, 'workspace/executeCommand', params, on_with_hints_results)
+      vim.lsp.buf_request(0, 'workspace/executeCommand', params, on_results)
     end,
   })
   input:mount()
@@ -189,7 +152,7 @@ function M.expr_search_hints()
           hints = hints,
         }},
       }
-      vim.lsp.buf_request(0, 'workspace/executeCommand', params, on_with_hints_results)
+      vim.lsp.buf_request(0, 'workspace/executeCommand', params, on_results)
     end,
   })
   input:mount()
